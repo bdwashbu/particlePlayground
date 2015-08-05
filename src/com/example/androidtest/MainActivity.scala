999ea524ac3a734f4ab2bad358cbaad140b833d9
bdwashbu <bdwashbu@gmail.com>
2015-08-04 22:42:41 -0500

    linear force

--- src/com/example/androidtest/MainActivity.scala
+++ src/com/example/androidtest/MainActivity.scala
@@ -40,30 +40,51 @@ object Screen {
   var height = 0
 }
 
-object Gravity {
-  var x = 0.0
-  var y = 0.0
-  var z = 0.0
+object Force {
+  var gravityX = 0.0
+  var gravityY = 0.0
+  var gravityZ = 0.0
+  var forceX = 0.0
+  var forceY = 0.0
+  var forceZ = 0.0
 }
 
-object ParticleSystem {
+object Model {
+  val particleSystem = ParticleSystem(500)
+}
+
+case class ParticleSystem(numParticles: Int) {
   
-  case class Particle(var x: Double,
+  val vbb = ByteBuffer.allocateDirect(numParticles * 2 * 4)
+  vbb.order(ByteOrder.nativeOrder()) // Use native byte order
+  val vertexBuffer = vbb.asFloatBuffer() // Convert from byte to float
+  
+  val sizebb = ByteBuffer.allocateDirect(numParticles * 4)
+  sizebb.order(ByteOrder.nativeOrder()) // Use native byte order
+  val sizeBuffer = sizebb.asFloatBuffer() // Convert from byte to float
+  
+  val colorbb = ByteBuffer.allocateDirect(numParticles * 4 * 4)
+  colorbb.order(ByteOrder.nativeOrder()) // Use native byte order
+  val colorBuffer = colorbb.asFloatBuffer() // Convert from byte to float
+  
+  abstract case class Particle(var x: Double,
                       var y: Double,
                       var xVel: Double,
                       var yVel: Double,
                       var size: Double,
-                      totalLife: Double,
-                      val next: (Particle, Double) => Unit,
-                      val onCollide: (Particle, Double) => Double) {
+                      totalLife: Double) {
     var currentLife: Double = totalLife
+    
+    def next(particle: Particle, elapsedTime: Double)
+    def onCollide(particle: Particle, value: Double): Double
   }
 
   var particles = new ListBuffer[Particle]()
   
   def explode(x: Double, y: Double) = {
-    ParticleSystem synchronized {
-      for(part <- 0 to 500) {
+    this synchronized {
+      particles.clear
+      for(part <- 0 until numParticles) {
         val rand = new Random()
         val newVelX = rand.nextDouble() * 2.0 - 1.0
         val newVelY = rand.nextDouble() * 2.0 - 1.0
@@ -74,58 +95,63 @@ object ParticleSystem {
         val size = rand.nextDouble() * 11.0 + 4.0
         val totalLife = rand.nextDouble() * 5.0 + 2.0
         
-        def next(particle: Particle, elapsedTime: Double) = {
-          var speed = Math.sqrt(particle.xVel*particle.xVel + particle.yVel*particle.yVel)
-          speed += 15000.0f / particle.size * elapsedTime
-          
-          particle.xVel *= speed
-          particle.yVel *= speed
-          
-          particle.currentLife -= speed * 0.005 * elapsedTime
-          particle.size = Math.max(particle.size * (particle.currentLife / particle.totalLife).toFloat, 1.0)
-        }
         
-        def onCollide(particle: Particle, value: Double) = {
-          value * -1.0
+        particles += new Particle(x, y, normVelX, normVelY, size, totalLife) {
+          def next(particle: Particle, elapsedTime: Double) = {
+            var speed = Math.sqrt(particle.xVel*particle.xVel + particle.yVel*particle.yVel)
+            speed += 15000.0f / particle.size * elapsedTime
+            
+            particle.xVel *= speed
+            particle.yVel *= speed
+            
+            particle.currentLife -= speed * 0.005 * elapsedTime
+            particle.size = Math.max(particle.size * (particle.currentLife / particle.totalLife).toFloat, 1.0)
+          }
+          
+          def onCollide(particle: Particle, value: Double) = {
+            value * -1.0
+          }
         }
-        
-        particles += Particle(x, y, normVelX, normVelY, size, totalLife, next, onCollide)
       }
     }
   }
   
   def freeFall(x: Double, y: Double) = {
-    ParticleSystem synchronized {
-      for(part <- 0 to 500) {
+    this synchronized {
+      particles.clear
+      for(part <- 0 until numParticles) {
         val rand = new Random()
-        val speed = rand.nextDouble() * 200.0 + 10.0
+        val speed = rand.nextDouble() * 300.0 + 60.0
         val newVelX = rand.nextDouble() * 2.0 - 1.0
         val newVelY = rand.nextDouble() * 2.0 - 1.0
         val dist = math.sqrt(newVelX * newVelX + newVelY * newVelY)
         
         val normVelX = newVelX / dist * speed
         val normVelY = newVelY / dist * speed
-        val size = rand.nextDouble() * 11.0 + 4.0
+        val size = rand.nextDouble() * 5.0 + 4.0
         val totalLife = rand.nextDouble() * 5.0 + 2.0
         
-        def next(particle: Particle, elapsedTime: Double) = {
-          particle.currentLife -= elapsedTime * 1.0
-          particle.xVel += -Gravity.x * elapsedTime * 300.0
-          particle.yVel += -Gravity.y * elapsedTime * 300.0
-        }
         
-        def onCollide(particle: Particle, value: Double) = {
-          value * -1.0 * (particle.size / 20.0)
+        particles += new Particle(x, y, normVelX, normVelY, size, totalLife) {
+          def next(particle: Particle, elapsedTime: Double) = {
+            particle.currentLife -= elapsedTime * 1.0
+            particle.xVel += -Force.gravityX * elapsedTime * 600.0
+            particle.yVel += -Force.gravityY * elapsedTime * 600.0
+            particle.xVel += -Force.forceX * elapsedTime * 6300.0
+            particle.yVel += -Force.forceY * elapsedTime * 6300.0
+          }
+          
+          def onCollide(particle: Particle, value: Double) = {
+            value * -1.0 * (particle.size / 20.0)
+          }
         }
-        
-        particles += Particle(x, y, normVelX, normVelY, size, totalLife, next, onCollide)
       }
     }
   }
   
   def run(elapsedTime: Double) = {
-    ParticleSystem synchronized {
-      particles.foreach { particle =>
+    this synchronized {
+      particles.par.foreach { particle =>
         
         var nextX = particle.x + particle.xVel * elapsedTime
         var nextY = particle.y + particle.yVel * elapsedTime
@@ -160,24 +186,27 @@ object ParticleSystem {
     }
   }
   
-  def getVertexArray: Array[Float] = {
-    ParticleSystem synchronized {
-      particles.flatMap{particle => List(particle.x.toFloat, particle.y.toFloat)}.toArray
-    }
+  def getVertexArray: FloatBuffer = {
+      vertexBuffer.clear()
+      vertexBuffer.put(particles.flatMap{particle => List(particle.x.toFloat, particle.y.toFloat)}.toArray) // Copy data into buffer
+      vertexBuffer.position(0) // Rewind
+      vertexBuffer
   }
   
-  def getColorArray: Array[Float] = {
-    ParticleSystem synchronized {
-      particles.flatMap { particle => 
+  def getColorArray: FloatBuffer = {
+      colorBuffer.clear
+      colorBuffer.put(particles.flatMap { particle => 
         List(1.0f, 1.0f, 1.0f, (particle.currentLife / particle.totalLife).toFloat)
-      }.toArray
-    }
+      }.toArray) // Copy data into buffer
+      colorBuffer.position(0) // Rewind
+      colorBuffer
   }
   
-  def getSizeArray: Array[Float] = {
-    ParticleSystem synchronized {
-      particles.map{particle => particle.size.toFloat * (particle.currentLife / particle.totalLife).toFloat}.toArray
-    }
+  def getSizeArray: FloatBuffer = {
+      sizeBuffer.clear
+      sizeBuffer.put(particles.map{particle => particle.size.toFloat * (particle.currentLife / particle.totalLife).toFloat}.toArray) // Copy data into buffer
+      sizeBuffer.position(0) // Rewind
+      sizeBuffer
   }
   
   
@@ -186,15 +215,24 @@ object ParticleSystem {
 
 class MainScala extends Activity with SensorEventListener {
   
+  
+  
   override def onAccuracyChanged(sensor: Sensor, accuracy: Int ) = {
       // not in use
   }
   
   override def onSensorChanged(event: SensorEvent) = {
     if (event.sensor.getType == Sensor.TYPE_GRAVITY) {
-     // Log.v("blah", event.values(0).toString);
-      Gravity.x = event.values(0)
-      Gravity.y = event.values(1)
+      //Log.v("blah", event.values(0).toString);
+      Force.gravityX = event.values(0)
+      Force.gravityY = event.values(1)
+//        gravity[0] = event.values[0];
+//        gravity[1] = event.values[1];
+//        gravity[2] = event.values[2];
+    } else if (event.sensor.getType == Sensor.TYPE_LINEAR_ACCELERATION) {
+      //Log.v("blah", event.values(0).toString);
+      Force.forceX = event.values(0)
+      Force.forceY = event.values(1)
 //        gravity[0] = event.values[0];
 //        gravity[1] = event.values[1];
 //        gravity[2] = event.values[2];
@@ -209,7 +247,7 @@ class MainScala extends Activity with SensorEventListener {
         case (MotionEvent.ACTION_DOWN) =>
           
             //ParticleSystem.explode(event.getX, 1920 - event.getY)
-            ParticleSystem.freeFall(event.getX, 1920 - event.getY)
+            Model.particleSystem.freeFall(event.getX, 1920 - event.getY)
             true
         case (MotionEvent.ACTION_MOVE) =>
           
@@ -242,7 +280,8 @@ class MainScala extends Activity with SensorEventListener {
     Screen.height = displaymetrics.heightPixels;
     Screen.width = displaymetrics.widthPixels;
     val mSensorManager = getSystemService(Context.SENSOR_SERVICE).asInstanceOf[SensorManager];
-    mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
+    mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_FASTEST);
+    mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_FASTEST);
   }
 
 }
@@ -275,37 +314,25 @@ class OpenGLRenderer extends Renderer {
     gl.glColor4f(0.0f, 0.0f, 1.0f, 1.0f)
     gl.glLoadIdentity()
     
+    
+    var vertexArray: FloatBuffer = null
+    var sizeArray: FloatBuffer = null
+    var colorArray: FloatBuffer = null
 
-    val array = ParticleSystem.getVertexArray
-    val sizeArray = ParticleSystem.getSizeArray
-    val colorArray = ParticleSystem.getColorArray
-    
-    val vbb = ByteBuffer.allocateDirect(array.length * 4)
-    vbb.order(ByteOrder.nativeOrder()) // Use native byte order
-    val vertexBuffer = vbb.asFloatBuffer() // Convert from byte to float
-    vertexBuffer.put(array) // Copy data into buffer
-    vertexBuffer.position(0) // Rewind
-    
-    val sizebb = ByteBuffer.allocateDirect(sizeArray.length * 4)
-    sizebb.order(ByteOrder.nativeOrder()) // Use native byte order
-    val sizeBuffer = sizebb.asFloatBuffer() // Convert from byte to float
-    sizeBuffer.put(sizeArray) // Copy data into buffer
-    sizeBuffer.position(0) // Rewind
-    
-    val colorbb = ByteBuffer.allocateDirect(colorArray.length * 4)
-    colorbb.order(ByteOrder.nativeOrder()) // Use native byte order
-    val colorBuffer = colorbb.asFloatBuffer() // Convert from byte to float
-    colorBuffer.put(colorArray) // Copy data into buffer
-    colorBuffer.position(0) // Rewind
+    Model.particleSystem synchronized {
+      vertexArray = Model.particleSystem.getVertexArray
+      sizeArray = Model.particleSystem.getSizeArray
+      colorArray = Model.particleSystem.getColorArray
+    }
 
     gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
     gl.glEnableClientState(GL11.GL_POINT_SIZE_ARRAY_OES)
     gl.glEnableClientState(GL10.GL_COLOR_ARRAY)
     
-    gl.glVertexPointer(2, GL10.GL_FLOAT, 0, vertexBuffer)
-    gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer)
-    gl.asInstanceOf[GL11].glPointSizePointerOES(GL10.GL_FLOAT, 0, sizeBuffer)
-    gl.glDrawArrays(GL10.GL_POINTS, 0, array.size / 2)
+    gl.glVertexPointer(2, GL10.GL_FLOAT, 0, vertexArray)
+    gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorArray)
+    gl.asInstanceOf[GL11].glPointSizePointerOES(GL10.GL_FLOAT, 0, sizeArray)
+    gl.glDrawArrays(GL10.GL_POINTS, 0, 500)
 
     gl.glDisableClientState(GL11.GL_POINT_SIZE_ARRAY_OES)
     gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)
@@ -313,7 +340,7 @@ class OpenGLRenderer extends Renderer {
     
     val elapsedTime = (SystemClock.elapsedRealtime - startTime) / 1000.0
     
-    ParticleSystem.run(elapsedTime)
+    Model.particleSystem.run(elapsedTime)
   }
 
   def onSurfaceChanged(gl: GL10, width: Int, height: Int) {
